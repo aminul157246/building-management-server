b70840edb4405bda01aa2eae32e3b23ff1e43907
aminul157246 <aminul.islam.157246@gmail.com>
2023-12-02 23:33:13 +0600

    get data fron db

---
+++ index.js
@@ -0,0 +1,70 @@
+// building_management
+// yrVRkFjdFzgVas8v
+
+
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// require('dotenv').config()
+const port = process.env.PORT || 5000
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+const uri = `mongodb+srv://building_management:yrVRkFjdFzgVas8v@cluster0.6v8amsy.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+
+      
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      
+      
+      const appartmentCollections = client.db("appartment").collection("appartmentCollection");
+
+
+      app.get('/appartment', async(req, res) => {
+        const result = await appartmentCollections.find().toArray()
+        res.send(result)
+       })
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('CRUD  is running ......!')
+})
+
+app.listen(port, () => {
+  console.log(`App is  listening on port ${port}`)
+})
