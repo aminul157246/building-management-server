70a47c34a25394e5cc6ef6116243f48006636b68
aminul157246 <aminul.islam.157246@gmail.com>
2023-12-04 01:01:35 +0600

    agreement and details part

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-// require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000
 
 //middleware
@@ -15,7 +15,7 @@ app.use(express.json())
 
 
 
-const uri = `mongodb+srv://building_management:yrVRkFjdFzgVas8v@cluster0.6v8amsy.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASS}@cluster0.6v8amsy.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -36,10 +36,45 @@ async function run() {
       
       
       const appartmentCollections = client.db("appartment").collection("appartmentCollection");
+      const agreementCollection = client.db("agreement").collection("agreementDB")
+      const detailsCollection = client.db('details').collection('detailsDB')
 
 
       app.get('/appartment', async(req, res) => {
-        const result = await appartmentCollections.find().toArray()
+        console.log(req.query);
+        const page = parseInt(req.query.page)
+        const size = parseInt(req.query.size)
+        const result = await appartmentCollections.find()
+        .skip(page * size)
+        .limit(size)
+        .toArray()
+        res.send(result)
+       })
+
+
+      app.get('/details', async(req, res) => {
+        const result = await detailsCollection.find().toArray()
+        res.send(result)
+       })
+
+       app.get('/details/:id', async(req, res) => {
+        const id = req.params.id
+        const query = {_id : new ObjectId(id)}
+        const result = await detailsCollection.findOne(query)
+        res.send(result)
+       })
+
+
+
+    //    agreement add to cart 
+       app.post('/agreement', async(req, res) => {
+        const cartItem = req.body;
+        const result = await agreementCollection.insertOne(cartItem)
+        res.send(result)
+       })
+
+       app.get('/agreement' , async(req, res) => {
+        const result = await agreementCollection.find().toArray()
         res.send(result)
        })
 
